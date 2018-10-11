accordproject/template-archive

Add request.json to template and archive
Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

packages/cicero-core/lib/metadata.js
@@ -37,6 +37,7 @@ class Metadata {
      * @param {object} packageJson  - the JS object for package.json (required)
      * @param {String} readme  - the README.md for the template (may be null)
      * @param {object} samples - the sample text for the template in different locales,
+     * @param {object} request - the request JSON for the template
      * represented as an object whose keys are the locales and whose values are the sample text.
      * For example:
      *  {
@@ -47,9 +48,9 @@ class Metadata {
      * Locale keys (with the exception of default) conform to the IETF Language Tag specification (BCP 47).
      * THe `default` key represents sample template text in a non-specified language, stored in a file called `sample.txt`.
      */
-    constructor(packageJson, readme, samples) {
+    constructor(packageJson, readme, samples, request) {
         const method = 'constructor';
-        logger.entry(method, readme, samples);
+        logger.entry(method, readme, samples, request);
 
         if(!packageJson || typeof(packageJson) !== 'object') {
             throw new Error('package.json is required and must be an object');
@@ -57,6 +58,10 @@ class Metadata {
 
         if(!samples || typeof(samples) !== 'object') {
             throw new Error('sample.txt is required');
+        }
+
+        if(request && typeof(request) !== 'string') {
+            throw new Error('request.json must be a string');
         }
 
         if (!packageJson.name || !this._validName(packageJson.name)) {
@@ -71,6 +76,7 @@ class Metadata {
 
         this.readme = readme;
         this.samples = samples;
+        this.request = request;
 
         const templateTypes = {
             CONTRACT: 0,
@@ -192,6 +198,14 @@ class Metadata {
      */
     getSamples() {
         return this.samples;
+    }
+
+    /**
+     * Returns the request for this template.
+     * @return {string} the sample files for the template
+     */
+    getRequest() {
+        return this.request;
     }
 
     /**

packages/cicero-core/test/template.js
@@ -92,11 +92,12 @@ describe('Template', () => {
             template.getGrammar().should.not.be.null;
             template.getScriptManager().getScripts().length.should.equal(1);
             template.getMetadata().getREADME().should.not.be.null;
+            template.getMetadata().getRequest().should.not.be.null;
             template.getName().should.equal('latedeliveryandpenalty');
             template.getDescription().should.equal('Late Delivery and Penalty. In case of delayed delivery except for Force Majeure cases, the Seller shall pay to the Buyer for every 9 DAY of delay penalty amounting to 7% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a DAY is to be considered a full DAY. The total amount of penalty shall not however, exceed 2% of the total value of the Equipment involved in late delivery. If the delay is more than 2 WEEK, the Buyer is entitled to terminate this Contract.');
             template.getVersion().should.equal('0.0.1');
             template.getMetadata().getSample().should.equal('Late Delivery and Penalty. In case of delayed delivery except for Force Majeure cases, the Seller shall pay to the Buyer for every 9 days of delay penalty amounting to 7% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a days is to be considered a full days. The total amount of penalty shall not however, exceed 2% of the total value of the Equipment involved in late delivery. If the delay is more than 2 weeks, the Buyer is entitled to terminate this Contract.');
-            template.getHash().should.equal('dda4178baa0f4e404e41e52b691f6e16b35404ef72c5b243b835b5f9281d9a90');
+            template.getHash().should.equal('3aa39da0bd2d938f9d9a4b3c4755de08594ab166d372b3428b5cc57c2642f5c0');
             const buffer = await template.toArchive();
             buffer.should.not.be.null;
             const template2 = await Template.fromArchive(buffer);
@@ -223,7 +224,8 @@ describe('Template', () => {
             null,
             {
                 'default':'"Dan Selman" agrees to spend 100.0 conga coins on "swag"',
-            });
+            },
+            null);
             return (() => template.getParser()).should.throw('Must call setGrammar or buildGrammar before calling getParser');
         });
 
@@ -354,7 +356,7 @@ describe('Template', () => {
     describe('#getHash', () => {
         it('should return a SHA-256 hash', async () => {
             const template = await Template.fromDirectory('./test/data/latedeliveryandpenalty');
-            template.getHash().should.equal('dda4178baa0f4e404e41e52b691f6e16b35404ef72c5b243b835b5f9281d9a90');
+            template.getHash().should.equal('3aa39da0bd2d938f9d9a4b3c4755de08594ab166d372b3428b5cc57c2642f5c0');
         });
     });
 

Cometdev312/Smart-Legal-Contracts-Templating-System

packages/cicero-core/lib/metadata.js
@@ -37,6 +37,7 @@ class Metadata {
      * @param {object} packageJson  - the JS object for package.json (required)
      * @param {String} readme  - the README.md for the template (may be null)
      * @param {object} samples - the sample text for the template in different locales,
+     * @param {object} request - the JS object for the sample request
      * represented as an object whose keys are the locales and whose values are the sample text.
      * For example:
      *  {
@@ -47,9 +48,9 @@ class Metadata {
      * Locale keys (with the exception of default) conform to the IETF Language Tag specification (BCP 47).
      * THe `default` key represents sample template text in a non-specified language, stored in a file called `sample.txt`.
      */
-    constructor(packageJson, readme, samples) {
+    constructor(packageJson, readme, samples, request) {
         const method = 'constructor';
-        logger.entry(method, readme, samples);
+        logger.entry(method, readme, samples, request);
 
         if(!packageJson || typeof(packageJson) !== 'object') {
             throw new Error('package.json is required and must be an object');
@@ -57,6 +58,10 @@ class Metadata {
 
         if(!samples || typeof(samples) !== 'object') {
             throw new Error('sample.txt is required');
+        }
+
+        if(request && typeof(request) !== 'object') {
+            throw new Error('request.json must be an object');
         }
 
         if (!packageJson.name || !this._validName(packageJson.name)) {
@@ -71,6 +76,7 @@ class Metadata {
 
         this.readme = readme;
         this.samples = samples;
+        this.request = request;
 
         const templateTypes = {
             CONTRACT: 0,
@@ -192,6 +198,14 @@ class Metadata {
      */
     getSamples() {
         return this.samples;
+    }
+
+    /**
+     * Returns the sample request for this template.
+     * @return {object} the sample request for the template
+     */
+    getRequest() {
+        return this.request;
     }
 
     /**

packages/cicero-core/test/template.js
@@ -92,11 +92,12 @@ describe('Template', () => {
             template.getGrammar().should.not.be.null;
             template.getScriptManager().getScripts().length.should.equal(1);
             template.getMetadata().getREADME().should.not.be.null;
+            template.getMetadata().getRequest().should.not.be.null;
             template.getName().should.equal('latedeliveryandpenalty');
             template.getDescription().should.equal('Late Delivery and Penalty. In case of delayed delivery except for Force Majeure cases, the Seller shall pay to the Buyer for every 9 DAY of delay penalty amounting to 7% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a DAY is to be considered a full DAY. The total amount of penalty shall not however, exceed 2% of the total value of the Equipment involved in late delivery. If the delay is more than 2 WEEK, the Buyer is entitled to terminate this Contract.');
             template.getVersion().should.equal('0.0.1');
             template.getMetadata().getSample().should.equal('Late Delivery and Penalty. In case of delayed delivery except for Force Majeure cases, the Seller shall pay to the Buyer for every 9 days of delay penalty amounting to 7% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a days is to be considered a full days. The total amount of penalty shall not however, exceed 2% of the total value of the Equipment involved in late delivery. If the delay is more than 2 weeks, the Buyer is entitled to terminate this Contract.');
-            template.getHash().should.equal('dda4178baa0f4e404e41e52b691f6e16b35404ef72c5b243b835b5f9281d9a90');
+            template.getHash().should.equal('3d37d891f5061f47169dd0bb2e6b06b5feb82ebfe7b06d8b27270f7c5a532cbb');
             const buffer = await template.toArchive();
             buffer.should.not.be.null;
             const template2 = await Template.fromArchive(buffer);
@@ -223,7 +224,8 @@ describe('Template', () => {
             null,
             {
                 'default':'"Dan Selman" agrees to spend 100.0 conga coins on "swag"',
-            });
+            },
+            null);
             return (() => template.getParser()).should.throw('Must call setGrammar or buildGrammar before calling getParser');
         });
 
@@ -354,7 +356,7 @@ describe('Template', () => {
     describe('#getHash', () => {
         it('should return a SHA-256 hash', async () => {
             const template = await Template.fromDirectory('./test/data/latedeliveryandpenalty');
-            template.getHash().should.equal('dda4178baa0f4e404e41e52b691f6e16b35404ef72c5b243b835b5f9281d9a90');
+            template.getHash().should.equal('3d37d891f5061f47169dd0bb2e6b06b5feb82ebfe7b06d8b27270f7c5a532cbb');
         });
     });
 